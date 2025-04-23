be55eaf7f29a2c9eac6201f2996b994cf52420ad
aaroj <aaro.jylhamaki@outlook.com>
2025-04-24 01:34:00 +0300

    refactor: standardize table variable names and enhance data fetching logic for menu items and categories

--- src/api/models/info_model.js
+++ src/api/models/info_model.js
@@ -1,49 +1,93 @@
 import promisePool from '../../utils/database.js';
-const menuTable = 'menu';
-const allergensTable = 'allergens';
-const restaurantTable = 'restaurant_info';
-const boardgamesTable = 'boardgames';
-const itemCategoriesTable = 'item_categories';
-const gameCategoriesTable = 'game_category_table';
+const menu = 'menu';
+const allergens = 'allergens';
+const allergenTable = 'allergen_table';
+const restaurant = 'restaurant_info';
+const boardgames = 'boardgames';
+const itemCategories = 'item_categories';
+const itemCategoriesTable = 'category_table';
+const gameCategories = 'game_category_table';
 
 const getMenuFromDatabase = async (lang) => {
+  let result = [];
+
+  // Fetch all menu items
   const [rows] = await promisePool.query(
-    `SELECT * FROM ${menuTable} WHERE lang="${lang}"`
+    `SELECT * FROM ${menu} WHERE lang="${lang}"`
   );
-  return rows;
+
+  // Fetch all allergens for each menu item
+  for (const row of rows) {
+    const allergenRows = await promisePool.query(
+      `SELECT * FROM ${allergenTable} WHERE menu_item_id=${row.id}`
+    );
+
+    let allergensList = [];
+    for (const allergen_id of allergenRows[0]) {
+      let allergen = await promisePool.query(
+        `SELECT * FROM ${allergens} WHERE id=${allergen_id.allergen_id}`
+      );
+
+      allergensList.push(allergen[0][0].name);
+    }
+
+    row.allergens = allergensList;
+    result.push(row);
+  }
+
+  // Fetch all categories for each menu item
+  for (const row of rows) {
+    const categoryRows = await promisePool.query(
+      `SELECT * FROM ${itemCategoriesTable} WHERE menu_item_id=${row.id}`
+    );
+    console.log(categoryRows);
+
+    let categoriesList = [];
+    for (const category_id of categoryRows[0]) {
+      let category = await promisePool.query(
+        `SELECT * FROM ${itemCategories} WHERE id=${category_id.category_id}`
+      );
+
+      categoriesList.push(category[0][0].name);
+    }
+
+    row.categories = categoriesList;
+  }
+
+  return result;
 };
 
 const getAllergensFromDatabase = async (lang) => {
   const [rows] = await promisePool.query(
-    `SELECT * FROM ${allergensTable} WHERE lang="${lang}"`
+    `SELECT * FROM ${allergens} WHERE lang="${lang}"`
   );
   return rows;
 };
 
 const getRestaurantFromDatabase = async (lang) => {
   const [rows] = await promisePool.query(
-    `SELECT * FROM ${restaurantTable} WHERE lang="${lang}"`
+    `SELECT * FROM ${restaurant} WHERE lang="${lang}"`
   );
   return rows;
 };
 
 const getBoardgamesFromDatabase = async (lang) => {
   const [rows] = await promisePool.query(
-    `SELECT * FROM ${boardgamesTable} WHERE lang="${lang}"`
+    `SELECT * FROM ${boardgames} WHERE lang="${lang}"`
   );
   return rows;
 };
 
 const getItemCategoriesFromDatabase = async (lang) => {
   const [rows] = await promisePool.query(
-    `SELECT * FROM ${itemCategoriesTable} WHERE lang="${lang}"`
+    `SELECT * FROM ${itemCategories} WHERE lang="${lang}"`
   );
   return rows;
 };
 
 const getGameCategoriesFromDatabase = async (lang) => {
   const [rows] = await promisePool.query(
-    `SELECT * FROM ${gameCategoriesTable} WHERE lang="${lang}"`
+    `SELECT * FROM ${gameCategories} WHERE lang="${lang}"`
   );
   return rows;
 };
